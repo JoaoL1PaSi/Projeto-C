c8b0076e3c77a1b0708102614c36dc179fa85af4
JoaoL1PaSi <ash1campeao24@gmail.com>
2026-03-13 19:41:03 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,22 +4,30 @@
 
 int main() {
     Contato agenda[100];
-    int totalContatos = 0; 
-    int opcao;
-
+    int totalContatos = 0, opcao, verifi;
+    
     do {
         printf("\n --- GERENCIADOR DE CONTATOS --- \n\n");
         printf("1. Cadastrar\n2. Listar\n3. Buscar\n4. Editar\n5. Excluir\n6. Estatisticas (Recursivo)\n7. Sair\n\n");
         printf("Escolha: ");
-        scanf("%d", &opcao);
-
+        
+        verifi = scanf("%d", &opcao);
+        
+		if (verifi != 1) {
+		    printf("\nERRO: Digite apenas NUMEROS!\n");
+		    while (getchar() != '\n');
+		    continue;
+		}
+		
         switch(opcao) {
             case 1:
-                cadastrarContato(agenda, &totalContatos);
+                cadastrar(agenda, &totalContatos);
+                printf("\n\n");
                 break;
 
             case 2:
-                listarContatos(agenda, totalContatos);
+                listar(agenda, totalContatos);
+                printf("\n\n");
                 break;
 
             case 3:
@@ -27,34 +35,42 @@ int main() {
                     char busca[50];
                     printf("Nome para busca: ");
                     scanf(" %[^\n]", busca);
-                    int i = buscarContato(agenda, totalContatos, busca);
-                    if(i != -1) 
-                        printf("Encontrado: %s | %s\n", agenda[i].nome, agenda[i].telefone);
-                    else 
-                        printf("Nao encontrado.\n");
+                    int i = buscar(agenda, totalContatos, busca);
+                    if(i != -1) {
+					    printf("\n--- Busca de Contato ---\n\n");
+					    printf("Nome: %s\n", agenda[i].nome);
+					    printf("Telefone: %s\n", agenda[i].telefone);
+					    printf("E-mail: %s\n", agenda[i].email);
+					} else {
+					    printf("Contato '%s' nao encontrado.\n", busca);
+					}
                 }
+                printf("\n\n");
                 break;
 
             case 4:
-                editarContato(agenda, totalContatos);
+                editar(agenda, totalContatos);
+                printf("\n\n");
                 break;
 
             case 5:
-                excluirContato(agenda, &totalContatos);
+                excluir(agenda, &totalContatos);
+                printf("\n\n");
                 break;
 
             case 6:
-                printf("Contatos com nomes longos (> 5 letras): %d\n", 
-                        contarNomesLongos(agenda, totalContatos, 0, 5));
+                printf("Contatos com nomes longos (> 5 letras): %d\n", contarNomesLongos(agenda, totalContatos, 0, 5));
+                printf("\n\n");
                 break;
 
             case 7:
+            	system("cls");
                 printf("Saindo...\n");
                 break;
 
             default:
                 printf("\nOpcao invalida!\n");
-                printf("______________________________________\n\n\n\n");
+                printf("\n______________________________________\n\n\n\n");
         }
     } while (opcao != 7);
 
